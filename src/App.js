fced91da79ca4c4fbd93c4eb4829869f91c4947c
Your Name <anishpandey021@gmail.com>
2021-11-09 22:26:34 +0545

    s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import react, { useState, useEffect } from "react";
+import react, { useState } from "react";
 import JokeItem from "./components/JokeItem";
+import "./components/style.css";
 function App() {
   return (
     <>
-      <JokeItem />
-      <JokeItem />
-      <JokeItem />
+      <div className="navbar">
+        <div className="item" id="item1">
+          Category
+        </div>
+        <div className="item" id="item2">
+          sasd
+        </div>
+        <div className="item" id="item3">
+          sasd
+        </div>
+        <div className="item" id="item4">
+          sasd
+        </div>
+      </div>
+      <JokeItem category="Any" />
     </>
   );
 }
--- src/components/JokeItem.js
+++ src/components/JokeItem.js
@@ -1,40 +1,62 @@
-// eslint-disable-next-line
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import loadingLogo from "./loader.svg";
 
-export default function JokeItem() {
+function repeatstringnumtimes(str, num) {
+  var repeatedstring = "";
+  for (let i = 0; i < num; i++) {
+    repeatedstring = repeatedstring + str;
+  }
+
+  return repeatedstring;
+}
+export default function JokeItem(props) {
   const [joke, setJoke] = useState("");
   const [Fetch, setFetch] = useState(true);
   const [Loading, setLoading] = useState(false);
   useEffect(() => {
     const getJoke = async () => {
       setLoading(true);
-      const jokeData = await fetch("https://v2.jokeapi.dev/joke/Any");
+      const jokeData = await fetch(
+        `https://v2.jokeapi.dev/joke/${props.category}`
+      );
       setLoading(false);
+
       const jokeObj = await jokeData.json();
       setJoke(
         jokeObj.joke
           ? jokeObj.joke
-          : `${jokeObj.setup} ---------${jokeObj.delivery} `
+          : [
+              `A:${jokeObj.setup} `,
+              repeatstringnumtimes("x", 37 - jokeObj.setup.length),
+              `B:${jokeObj.delivery}`,
+            ]
       );
+      console.log(Array.isArray(joke));
+      console.log(joke);
     };
     getJoke();
   }, [Fetch]);
   return (
-    <>
-      <div className="test"> JOKE-POKE</div>
-      <button
-        className="btn-primary"
-        onClick={() => {
-          setFetch(!Fetch);
-          console.log(Fetch);
-        }}
-      >
-        Get A Joke
-      </button>
-
-      {Loading ? <img src={loadingLogo} alt="loading" /> : <div>{joke}</div>}
-    </>
+    <div className="JokeBox">
+      <div className="heading">Get A Joke</div>
+      <div className="mainjoke">
+        {Loading ? (
+          <img src={loadingLogo} alt="loading" />
+        ) : (
+          <div>{!Array.isArray(joke) ? joke : joke[0] + joke[2]}</div>
+        )}
+      </div>
+      <div className="btnarea">
+        <button
+          className="btn-primary"
+          onClick={() => {
+            setFetch(!Fetch);
+          }}
+        >
+          Another One
+        </button>
+      </div>
+    </div>
   );
 }
