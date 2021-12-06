a82cafaac10104f9cff2fcb78c5d247ec9f7de93
Your Name <anishpandey021@gmail.com>
2021-12-06 21:11:49 +0545

    hello

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import react, { useState } from "react";
 import JokeItem from "./components/JokeItem";
 import "./components/style.css";
+import settinglogo from "./components/assets/settings.png";
+// document.body.style.background = " red ";
+
 function App() {
+  const [Theme, setTheme] = useState(true);
+
   return (
     <>
-      <div className="navbar">
-        <div className="item" id="item1">
-          Category
-        </div>
-        <div className="item" id="item2">
-          sasd
-        </div>
-        <div className="item" id="item3">
-          sasd
-        </div>
-        <div className="item" id="item4">
-          sasd
+      <div className="customizeArea">
+        <div className="options">
+          <div
+            className="theme"
+            title={Theme ? "Switch to light theme" : "Switch to dark theme"}
+          >
+            <svg
+              onClick={() => {
+                if (Theme) {
+                  document.body.classList.remove("darkStyle");
+                  document.body.classList.add("lightStyle");
+                } else {
+                  document.body.classList.remove("lightStyle");
+                  document.body.classList.add("darkStyle");
+                }
+                console.log(Theme);
+                setTheme(!Theme);
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill={Theme ? "white" : "black"}
+              className="bi bi-lightbulb-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z" />
+            </svg>
+          </div>
+          <div className="settings">
+            <img src={settinglogo} alt="Settings" />
+          </div>
         </div>
       </div>
       <JokeItem category="Any" />
--- src/components/JokeItem.js
+++ src/components/JokeItem.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import loadingLogo from "./loader.svg";
+// import { themelogo } from "./assets/theme.png";
 
 function repeatstringnumtimes(str, num) {
   var repeatedstring = "";
@@ -10,6 +11,7 @@ function repeatstringnumtimes(str, num) {
 
   return repeatedstring;
 }
+
 export default function JokeItem(props) {
   const [joke, setJoke] = useState("");
   const [Fetch, setFetch] = useState(true);
@@ -28,35 +30,36 @@ export default function JokeItem(props) {
           ? jokeObj.joke
           : [
               `A:${jokeObj.setup} `,
-              repeatstringnumtimes("x", 37 - jokeObj.setup.length),
+              repeatstringnumtimes(". ", 10),
               `B:${jokeObj.delivery}`,
             ]
       );
-      console.log(Array.isArray(joke));
-      console.log(joke);
     };
     getJoke();
   }, [Fetch]);
   return (
-    <div className="JokeBox">
-      <div className="heading">Get A Joke</div>
-      <div className="mainjoke">
-        {Loading ? (
-          <img src={loadingLogo} alt="loading" />
-        ) : (
-          <div>{!Array.isArray(joke) ? joke : joke[0] + joke[2]}</div>
-        )}
+    <>
+      <div className="JokeBox">
+        <div className="heading">Get A Joke</div>
+        <div className="mainjoke">
+          {Loading ? (
+            <img src={loadingLogo} alt="loading" />
+          ) : (
+            <div>{!Array.isArray(joke) ? joke : joke[0] + joke[2]}</div>
+          )}
+        </div>
+
+        <div className="btnarea">
+          <button
+            className="btn-primary"
+            onClick={() => {
+              setFetch(!Fetch);
+            }}
+          >
+            Another One
+          </button>
+        </div>
       </div>
-      <div className="btnarea">
-        <button
-          className="btn-primary"
-          onClick={() => {
-            setFetch(!Fetch);
-          }}
-        >
-          Another One
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
